5a83d09b2a483118ac9899be62534837b1bf4671
Alex Parras <aph421@gmail.com>
2025-04-06 21:08:47 +0200

    Instalando y configurando tailwind y flowbite

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,19 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {initFlowbite} from 'flowbite';
+import {NavbarComponent} from './components/navbar/navbar.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NavbarComponent],
   templateUrl: './app.component.html',
+  standalone: true,
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
+  ngOnInit(): void {
+     initFlowbite()
+  }
   title = 'Piezas-muertas';
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,20 @@
 import { Routes } from '@angular/router';
+import {AppComponent} from './app.component';
+import {MainComponent} from './components/main/main.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: '',
+    pathMatch: 'full',
+  },
+  {
+    path: '',
+    component: MainComponent,
+  },
+  {
+    path: '**',
+    redirectTo: 'main',
+    pathMatch: 'full',
+  }
+];
