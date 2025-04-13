ee2a5a72f9800ac0069b0cb1cb7f1e0d1869734e
Alex Parras <aph421@gmail.com>
2025-04-13 12:09:46 +0200

    añadiendo footer y ajustando el grid

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,10 +2,11 @@ import {Component, OnInit} from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import {initFlowbite} from 'flowbite';
 import {NavbarComponent} from './components/navbar/navbar.component';
+import {FooterComponent} from './components/footer/footer.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, NavbarComponent],
+  imports: [RouterOutlet, NavbarComponent, FooterComponent],
   templateUrl: './app.component.html',
   standalone: true,
   styleUrl: './app.component.css'
